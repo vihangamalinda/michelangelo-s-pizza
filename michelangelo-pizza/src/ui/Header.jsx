a08247a80984e42a943e5f55808b7804223d985e
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-20 03:48:31 -0800

    feat: Header

---
+++ michelangelo-pizza/src/ui/Header.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+function Header(){
+    return(
+        <header>
+            <Link to="/">To Michelangelo&apos;s Pizz 🍕</Link>
+            <p>Vihanga</p>
+        </header>
+    )
+}
+export default Header;
