42696250a40c6a1bc2167eb3b54ecdd971bbef66
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-20 06:40:24 -0800

    feat: Using global navigation to identify if we are still loading data using router.

--- michelangelo-pizza/src/ui/AppLayout.jsx
+++ michelangelo-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader"
 import Header from "./Header";
 
 function AppLayout(){
-    return <div>
+    const navigation = useNavigation()
+    const isLoading = navigation.state === "loading"
+
+    return <div className="layout">
+        {isLoading && <Loader/>}
+
         <Header/>
         <main>
             <Outlet/>
