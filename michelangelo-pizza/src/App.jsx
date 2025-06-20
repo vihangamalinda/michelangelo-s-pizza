4b80e259914dca53702e9acff7173e1b12a46d0b
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-20 03:01:39 -0800

    feat: Adding routes

--- michelangelo-pizza/src/App.jsx
+++ michelangelo-pizza/src/App.jsx
@@ -1,11 +1,36 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import Order from "./features/order/Order"
+import CreateOrder from './features/order/CreateOrder'
 import './App.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-    <h2>HI</h2>
-</>
-  )
+  return <RouterProvider router={router}/>;
 }
 
 export default App
