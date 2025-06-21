d260d9deccb7fdc2a3a7c44b92c5b743d507a19a
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-20 21:41:58 -0800

    fix: adding search-order

--- michelangelo-pizza/src/ui/Header.jsx
+++ michelangelo-pizza/src/ui/Header.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
+import SearchOrder from "../features/order/SearchOrder";
 function Header(){
     return(
         <header>
             <Link to="/">To Michelangelo&apos;s Pizz 🍕</Link>
+            <SearchOrder/>
             <p>Vihanga</p>
         </header>
     )
