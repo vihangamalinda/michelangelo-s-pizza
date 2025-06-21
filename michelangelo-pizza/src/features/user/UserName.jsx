06680b4ace9d0d4549a6b0489902e965fd995723
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-21 05:17:23 -0800

    feat: Using tailwind flexbox class to display

--- michelangelo-pizza/src/features/user/UserName.jsx
+++ michelangelo-pizza/src/features/user/UserName.jsx
@@ -1,4 +1,4 @@
 function UserName() {
-  return <div className="hidden text-sm font-semibold"> Vihanga</div>;
+  return <div className="hidden text-sm font-semibold md:block"> Vihanga</div>;
 }
 export default UserName;
