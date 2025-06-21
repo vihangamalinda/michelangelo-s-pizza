6062e5abbaec7a060d51e3d0303f6dd99a7d7f17
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-20 21:42:34 -0800

    feat: Fetching the data for a order by using router.

--- michelangelo-pizza/src/App.jsx
+++ michelangelo-pizza/src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
 import Home from "./ui/Home"
 import Menu,{loader as menuLoader} from "./features/menu/Menu"
 import Cart from "./features/cart/Cart"
-import Order from "./features/order/Order"
+import Order,{loader as orderLoader} from "./features/order/Order"
 import CreateOrder from './features/order/CreateOrder'
 import Error from "./ui/Error"
 import './App.css'
@@ -34,6 +34,8 @@ const router = createBrowserRouter([
   {
     path: "/order/:orderId",
     element: <Order />,
+    loader:orderLoader,
+    errorElement:<Error/>
   },
     ]
   },
