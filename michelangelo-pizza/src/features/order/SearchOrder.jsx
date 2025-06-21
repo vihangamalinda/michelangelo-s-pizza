1e805d3eb17c3f023c419427d0f5790f18f62118
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-21 09:42:24 -0800

    feat: Adding style classes for search-order input field.

--- michelangelo-pizza/src/features/order/SearchOrder.jsx
+++ michelangelo-pizza/src/features/order/SearchOrder.jsx
@@ -17,6 +17,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-amber-100 px-4 py-2 transition-all duration-300 placeholder:text-stone-400 focus:w-32 focus:outline-none focus:ring focus:ring-amber-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
