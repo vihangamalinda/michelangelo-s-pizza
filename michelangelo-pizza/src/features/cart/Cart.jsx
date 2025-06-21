4fda343558d7f176c2a6204da38a45867855fa4f
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-21 08:49:55 -0800

    feat: adding hover state with styles.

--- michelangelo-pizza/src/features/cart/Cart.jsx
+++ michelangelo-pizza/src/features/cart/Cart.jsx
@@ -30,7 +30,12 @@ function Cart() {
 
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <Link
+        to="/menu"
+        className="text-sm text-blue-400 hover:font-bold hover:text-blue-600"
+      >
+        &larr; Back to menu
+      </Link>
 
       <h2>Your cart, %NAME%</h2>
 
