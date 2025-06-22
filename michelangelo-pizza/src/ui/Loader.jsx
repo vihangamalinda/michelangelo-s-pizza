8d70dcb2c81cf0a98a0fb407b5db4d1676657e17
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-22 02:32:23 -0800

    feat: styling the layer.

--- michelangelo-pizza/src/ui/Loader.jsx
+++ michelangelo-pizza/src/ui/Loader.jsx
@@ -1,4 +1,6 @@
 function Loader() {
-  return <div className="loader"></div>;
+  return <div className="absolute flex items-center justify-center bg-slate-200/20 backdrop-blur-sm inset-0">
+    <div className="loader"></div>
+    </div>;
 }
 export default Loader;
