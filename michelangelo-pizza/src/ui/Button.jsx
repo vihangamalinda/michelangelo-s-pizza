bd0e0ea24ff4a8fa7d6bddbb641ffcad3506a1b7
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-22 06:19:40 -0800

    feat: Styling the cart.

--- michelangelo-pizza/src/ui/Button.jsx
+++ michelangelo-pizza/src/ui/Button.jsx
@@ -6,7 +6,10 @@ function Button({ children, isDisabled, to, type }) {
 
   const styles = {
     primary: baseStyles + ' px-4 py-3 md:px-6 md:py-4',
+
     small: baseStyles + ' px-4 text-xs py-2 md:px-5 md:py-5',
+    secondary:
+      'tracking-wide inline-block px-4 py-2.5 md:px-6 md:py-3.5 rounded-full border-2 border-stone-200 font-semibold uppercase text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed focus:text-stone-800 ',
   };
   const className = styles[type];
   if (to) {
