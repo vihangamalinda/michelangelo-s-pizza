fed243c12a5438c5d8f52b483bf96d5ee981278d
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-29 12:55:56 -0800

    feat: Handling cart overview with redux state.

--- michelangelo-pizza/src/features/cart/Cart.jsx
+++ michelangelo-pizza/src/features/cart/Cart.jsx
@@ -3,34 +3,14 @@ import { Link } from 'react-router-dom';
 import CartItem from './CartItem';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function Cart() {
-  const cart = fakeCart;
-
+  const dispatch = useDispatch();
+  const cart = useSelector(getCart);
+  
   return (
     <div className="px-4 py-3">
       <LinkButton
@@ -52,7 +32,9 @@ function Cart() {
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={(e) => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
--- michelangelo-pizza/src/features/cart/CartOverview.jsx
+++ michelangelo-pizza/src/features/cart/CartOverview.jsx
@@ -1,11 +1,18 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getTotalCartItems, getTotalPrice } from './cartSlice';
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartItems);
+  const totalPrice = useSelector(getTotalPrice);
+
+  if(!totalCartQuantity) return null;
+
   return (
     <div className="flex items-center justify-between bg-zinc-800 px-4 py-4 text-sm uppercase text-zinc-200 sm:px-6 md:text-base">
       <p className="space-x-4 text-zinc-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>${totalPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
