b016bb8c4c7766c6b3bdd0fa00d92993ee7ff12a
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-02 14:16:24 -0800

    fix: MInor detail.

--- michelangelo-pizza/src/features/cart/Cart.jsx
+++ michelangelo-pizza/src/features/cart/Cart.jsx
@@ -10,6 +10,7 @@ import EmptyCart from './EmptyCart';
 function Cart() {
   const dispatch = useDispatch();
   const cart = useSelector(getCart);
+  const userName = useSelector(state=>state.user.userName);
 
   if (!cart.length) {
     return <EmptyCart />;
@@ -24,7 +25,7 @@ function Cart() {
         &larr; Back to menu
       </LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold"> Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold"> Your cart, {userName}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
